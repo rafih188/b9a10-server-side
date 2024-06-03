5f92b9ad99aa9677e50f49e9284489eafa213e4b
rafih188 <rafih1888@gmail.com>
2024-06-03 13:03:16 +0600

    'enablegetoperations'

--- index.js
+++ index.js
@@ -24,11 +24,23 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful 
-       
         // connection
+        const craftCollection = client.db('craftItems').collection('craftCard');
+
+      // get operations 
+      app.get('/craft', async (req, res) => {
+        const cursor = craftCollection.find();
+        if((await cursor.count)=== 0){
+            console.log('no data found');
+        }
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
         
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
